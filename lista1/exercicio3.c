783e5e6d330204187ea3bdc3e53e76ec91591380
SamuelCagnani <samuelcagnani@gmail.com>
2023-09-25 22:08:41 -0300

    Lista 1 Finalizada

--- lista1/exercicio3.c
+++ lista1/exercicio3.c
@@ -14,11 +14,16 @@ int main() {
     float n1 = 3.14;
     float n2 = 5.18;
 
-    if(n1 > n2) {
+    if(n1 > n2) 
+    {
         printf("%.2f eh maior que %.2f\n", n1 , n2);  
-    } else if(n1 < n2) {
+    } 
+    else if(n1 < n2) 
+    {
         printf("%.2f eh maior que %.2f\n", n2, n1);
-    } else {
+    } 
+    else 
+    {
         printf("Os valores são iguais!");
     }
 
